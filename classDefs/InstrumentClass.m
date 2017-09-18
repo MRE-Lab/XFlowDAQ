b035f88737615020415114042cb19fef2386b697
Ben Strom <strombw@Bens-MacBook-Pro.local>
2017-09-17 20:00:55 -0700

    this is a test

---
+++ classDefs/InstrumentClass.m
@@ -0,0 +1,9 @@
+classdef InstrumentClass
+    % Instrument class for v4 data aquisition
+    properties
+        name
+        turbineNum
+        dataStorageMethod
+        aquisitionRate
+    end
+end
